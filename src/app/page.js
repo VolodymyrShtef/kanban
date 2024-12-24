0ce84ccd992462e68dda2d4e7f6f36920de9f726
Volodymyr Shtefaniuk <volodimir.stefanyuk@gmail.com>
2024-12-24 14:34:20 +0200

    * add Suspense * change date format

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import KanbanBoard from "./screens/KanbanBoard";
 
 const Home = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] min-h-screen p-4 pt-2 gap-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex row-start-2">
-        <KanbanBoard />
+        <Suspense fallback={<div>Loading...</div>}>
+          <KanbanBoard />
+        </Suspense>
       </main>
     </div>
   );
--- src/app/store/useKanbanStore.js
+++ src/app/store/useKanbanStore.js
@@ -4,7 +4,7 @@ const useKanbanStore = () => {
       id: "1",
       title: "Додати Tailwind CSS, SnadcnUI, dndkit, zod",
       status: "TODO",
-      createdAt: new Date(),
+      createdAt: "2024-12-11",
     },
     {
       id: "2",
@@ -12,28 +12,28 @@ const useKanbanStore = () => {
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean lobortis.",
       status: "TODO",
-      createdAt: new Date(),
+      createdAt: "2024-12-12",
     },
     {
       id: "3",
       title: "Додати функціонал DND",
       status: "TODO",
-      createdAt: new Date(),
+      createdAt: "2024-12-14",
     },
     {
       id: "4",
       title: "Ініціалізація Next.js проекту",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       status: "IN_PROGRESS",
-      createdAt: new Date(),
-      dueDate: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+      createdAt: "2024-12-15",
+      dueDate: "2024-12-25",
     },
   ];
 
   const getTasks = () => {
-    const storedTasks = JSON.parse(localStorage.getItem("kanbanTasks"));
+    const storedTasks = localStorage.getItem("kanbanTasks");
 
-    return storedTasks?.length ? storedTasks : initialTasks;
+    return storedTasks ? JSON.parse(storedTasks) : initialTasks;
   };
 
   const saveTasks = (tasks) => {
